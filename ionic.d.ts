97c5eaa19c4f99b48f785eb3ade4968ee5d2b5c1
Mike Murray <MikeM.tba@gmail.com>
2022-04-13 08:16:12 +0200

    refactor: :rotating_light: Update linting

--- ionic.d.ts
+++ ionic.d.ts
@@ -1,12 +1,12 @@
-import { ReactText, HTMLAttributes } from "react"
-import { JSX as LocalJSX } from "@ionic/core"
-import { JSX as IoniconsJSX } from "ionicons"
+import { ReactText, HTMLAttributes } from 'react'
+import { JSX as LocalJSX } from '@ionic/core'
+import { JSX as IoniconsJSX } from 'ionicons'
 import IonicIntrinsicElements = LocalJSX.IntrinsicElements
 import IoniconsIntrinsicElements = IoniconsJSX.IntrinsicElements
 
 type ToReact<T> = {
   [P in keyof T]?: T[P] &
-    Omit<HTMLAttributes<Element>, "className"> & {
+    Omit<HTMLAttributes<Element>, 'className'> & {
       class?: string
       key?: ReactText
     }
