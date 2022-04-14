83dfcf6d9b0bcd360c119593ea317b52b76808fd
Mike Murray <mmurray@jhb.dvt.co.za>
2022-04-14 08:35:40 +0200

    feat: :sparkles: Implemented modal and 404

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function FourOhFour() {
+  return (
+    <>
+      <h1>Uh oh, page not found!</h1>
+      <div>404</div>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,30 @@
+import { useState } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import Modal from '../components/modal'
+import { getPlatforms } from '@ionic/react'
+import { IonButton } from '@ionic/react'
+import { isBrowser } from '../utils'
 
 const Home: NextPage = () => {
+  const [showModal, setShowModal] = useState(false)
+
+  isBrowser && console.log('getPlatforms():', getPlatforms())
+
   return (
     <>
       <Head>
         <title>Hennops Revival Home</title>
       </Head>
-      <main>
-        <h1>Welcome to Hennops Revival project!</h1>
-        <Link href="/new">Link to another page</Link>
-      </main>
+      <h1>Welcome to Hennops Revival project!</h1>
+      <Link href="/new">Link to another page</Link>
+      <IonButton onClick={() => setShowModal(true)}>Show Modal</IonButton>
+      <Modal header={'Butts'} isOpen={showModal} setState={setShowModal}>
+        <h2 className="text-red-500 text-6xl">Hello!</h2>
+        <h3>Subheading</h3>
+        <p>This is the modal content</p>
+      </Modal>
     </>
   )
 }
