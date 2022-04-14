5d92e182ee884af4e923db3d32921c250a61ceaa
Mike Murray <mmurray@jhb.dvt.co.za>
2022-04-14 08:31:14 +0200

    feat: :sparkles: Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./pages/**/*.tsx', './layouts/**/*.tsx', './components/**/*.tsx'],
+  darkMode: 'media',
+  theme: {
+    fontFamily: {},
+    extend: {},
+  },
+  plugins: [],
+}
